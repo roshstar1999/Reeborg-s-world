7666330aacb20e05ea6978333375dcde54dd2c19
N V Roshni <roshstar1999@gmail.com>
2023-04-16 12:20:02 +0530

    Create hurdle_4.py

---
+++ hurdle_4.py
@@ -0,0 +1,33 @@
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    turn_left()
+    while not right_is_clear():
+        move()
+    
+    turn_right()
+    move()
+    turn_right()
+    
+    while not wall_in_front():
+        move() 
+    turn_left()
+
+while not at_goal():
+    if  wall_in_front():
+        jump()
+    else:
+        move()
+
+    
+  
+
+   
+
+    
+
+    
+    
