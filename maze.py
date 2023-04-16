008a54915be01514d500ffc891308744290fd2b0
N V Roshni <roshstar1999@gmail.com>
2023-04-16 13:15:47 +0530

    Create maze.py

---
+++ maze.py
@@ -0,0 +1,69 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+while not is_facing_north():
+    turn_left()
+
+while not at_goal():
+     if  right_is_clear():
+        turn_right()
+        move()
+        
+     elif front_is_clear():
+        move()
+        
+     else:
+        turn_left()
+        
+   
+'''  
+ if  right_is_clear():
+        turn_right()
+        move()
+        turn_right()
+        move()
+while :
+    while right_is_clear():
+        turn_right
+        while front_is_clear():
+            move()
+        turn_left()
+    if wall_at_front():
+        turn_left
+      
+        
+    
+    
+    
+ 
+if right_is_clear():
+    turn_right()
+    while front_is_clear():
+        move()
+
+while not is_facing_north():
+    turn_left()
+    while front_is_clear()
+
+    
+turn_right()
+if right_is_clear():
+    turn_right()
+    if not wall_in_front():
+        move()
+    else:
+        turn
+
+'''    
+    
+
+    
+  
+
+   
+
+    
+
+    
+    
